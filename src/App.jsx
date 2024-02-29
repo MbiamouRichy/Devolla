96a108f120172e2920481bbeaf922bb1219bc7e9
MbiamouRichy <richymbiamou52@gmail.com>
2024-02-29 13:22:15 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import "./App.css";
+import Slider from "./components/slider";
 import Theme from "./components/theme";
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
-      <div className="dark:bg-black w-screen h-screen bg-white">
-        <Theme />
-      </div>
+      <main className="dark:bg-[#1b1b1f] flex flex-col w-screen h-screen bg-white">
+        <div className="flex col lg:flex-row justify-between items-center px-2 w-full lg:px-4 py-2">
+          <p className="dark:text-white text-[#1b1b1f] text-lg font-bold">
+            Devolla
+          </p>
+          <div className="flex flex-row gap-3">
+            <Button
+              varriant="outline"
+              size="icon"
+              className="bg-[#1b1b1f] hover:bg-[#1b1b1f] dark:bg-white text-white dark:text-[#000] rounded-full"
+            >
+              Eng
+            </Button>
+            <Theme />
+          </div>
+        </div>
+        <div className="flex-grow bg-slate-100 relative rounded-[20px] flex flex-row overflow-hidden mx-2 mb-3">
+          <Slider></Slider>
+        </div>
+      </main>
     </>
   );
 }
---
+++ src/components/slider.jsx
@@ -0,0 +1,73 @@
+import { ArrowRight } from "lucide-react";
+import { useState } from "react";
+import img1 from "../assets/1.jpg";
+import img10 from "../assets/10.jpg";
+import img2 from "../assets/2.jpg";
+import img3 from "../assets/3.jpg";
+import img4 from "../assets/4.jpg";
+import img5 from "../assets/5.jpg";
+import img6 from "../assets/6.jpg";
+import img7 from "../assets/7.jpg";
+import img8 from "../assets/8.jpg";
+import img9 from "../assets/9.jpg";
+
+let slides = [
+  { img: img1 },
+  { img: img2 },
+  { img: img3 },
+  { img: img4 },
+  { img: img5 },
+  { img: img6 },
+  { img: img7 },
+  { img: img8 },
+  { img: img9 },
+  { img: img10 },
+];
+export default function Slider() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  let [actifSlide, setActifSlide] = useState(0);
+  const handleClick = () => {
+    setActifSlide(actifSlide + 1);
+    if (actifSlide >= slides.length - 1) {
+      setActifSlide(0);
+    }
+  };
+
+  const handleMouseMove = (event) => {
+    setPosition({ x: event.clientX, y: event.clientY });
+  };
+  return (
+    <>
+      {/* slider */}
+
+      <div
+        style={{
+          top: `${position.y - 20}px`,
+          left: `${position.x - 20}px`,
+        }}
+        onClick={() => handleClick()}
+        className="dark:bg-[#1b1b1f] cursor-pointer bg-white rounded-full p-2 border-[2px] z-20 border-[black] dark:border-[#fff] fixed top-0 left-0"
+      >
+        {" "}
+        <ArrowRight className="h-10 w-10 dark:text-white"></ArrowRight>
+      </div>
+      <div
+        onMouseMove={handleMouseMove}
+        style={{ transform: `translateX(${actifSlide * -1}00%)` }}
+        className="w-full h-full flex flex-row relative transition-all duration-300"
+      >
+        {" "}
+        {slides.map((slide, idx) => (
+          <img
+            className={`w-full h-hull object-cover rounded-[20px] translate-x-[${
+              idx * -1
+            }00%]`}
+            src={slide.img}
+            key={idx}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
